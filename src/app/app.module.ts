daed5a43b841519fb6383134f4413b78d914326d
Johnny Proano <jproano22@yahoo.com>
2023-05-30 13:30:41 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BooksComponent } from './books/books.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+@NgModule(
+  {
+    declarations: [AppComponent, BooksComponent],
+    imports:[BrowserModule],
+    bootstrap: [AppComponent],
+  }
+)
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+
+export class AppModule{
+
+}
